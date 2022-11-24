261b122b3272723b2a2c0165be6bcf735fd14a29
Wojciech Majewski <wojciech.majewski@pm.me>
2022-11-24 18:30:20 +0100

    Map volume to right encoder

--- keymap.c
+++ keymap.c
@@ -89,6 +89,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+   if (index == 0) {
+      if (clockwise) {
+         tap_code(KC_PGDN);
+      } else {
+         tap_code(KC_PGUP);
+      }
+   } else if (index == 1) {
+      if (clockwise) {
+         tap_code(KC_VOLU);
+      } else {
+         tap_code(KC_VOLD);
+      }
+   }
+   return false;
+}
+#endif
+
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
